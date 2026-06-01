865aed16d1c3095690664ff9308b2921a6dff0c5
TheB <david@d-works.de>
2026-06-01 11:24:51 +0200

    new file:   VB365-JetToObjectMigration/VB365-JetToObjectMigration.ps1

---
+++ VB365-JetToObjectMigration/VB365-JetToObjectMigration.ps1
@@ -0,0 +1,81 @@
+<#
+.SYNOPSIS
+	Start the job based migration of Jet Repositories to Object Storage Repositories
+
+.DESCRIPTION
+  This script need to be run on the VB365 Controller. It reads the configuration to make a selection for a certain job possible.
+  After the selections have been made, the related proxy is getting modified and the migration started.
+
+.OUTPUTS
+	Start the Start-VBODataMigration process for a specific selected job, switch over to the new target and leaves the job disabled. 
+    Before re-enabling the job, the migration needs to be verified by using the verification script.
+
+.NOTES
+	NAME:  VB365-JetToObjectMigration.ps1
+	VERSION: 0.1
+	AUTHOR: David Bewernick
+	GITHUB: https://github.com/d-works
+
+#>
+
+#enable the migration option (on VB365 server)
+[Environment]::SetEnvironmentVariable("VEEAM_DATA_MIGRATION_ENABLED", "true")
+
+# Selection of organization, job, source and target repository
+
+# Organization selection
+Write-Host "Select Organization:"
+$orgs = Get-VBOOrganization | Sort-Object Name
+for($i=0; $i -lt $orgs.count; $i++) { Write-Host $i. $orgs[$i].name }
+$organisationNum = Read-Host "Enter organization number"
+$organization = $orgs[$organisationNum]
+Write-Host
+
+# Job selection
+Write-Host "Select Job:"
+$jobs = Get-VBOJob -Organization $organization | Sort-Object Name
+for($i=0; $i -lt $jobs.count; $i++) { Write-Host $i. $jobs[$i].name }
+$jobNum = Read-Host "Enter job number"
+$selectedJob = $jobs[$jobNum]
+$validationTarget = $selectedJob
+$validationType = "Job"
+$inventoryDataIdColumnName = "Backup Job Id"
+Write-Host
+
+# Source Repository selection
+Write-Host "Select Source Repository:"
+$sourceRepos = Get-VBORepository | Where-Object{($_.ObjectStorageRepository -eq $Null) -and (Get-VBOEntityData -Repository $_ -Type Organization -Name $organization.Name) -ne $Null} | Sort-Object Name
+for($i=0; $i -lt $sourceRepos.count; $i++) { Write-Host $i.  $sourceRepos[$i].name }
+$sourceRepoNum = Read-Host  "Enter Source repository number"
+$sourceRepository = $sourceRepos[$sourceRepoNum]
+Write-Host
+
+# Target Repository selection
+Write-Host "Select Target Repository:"
+$targetRepos = Get-VBORepository | Where-Object{($_.ObjectStorageRepository -ne $Null) -and (Get-VBOEntityData -Repository $_ -Type Organization -Name $organization.Name) -ne $Null} | Sort-Object Name
+for($i=0; $i -lt $targetRepos.count; $i++) { Write-Host $i.  $targetRepos[$i].name }
+$targetRepoNum = Read-Host  "Enter Target repository number"
+$targetRepository = $targetRepos[$targetRepoNum]
+Write-Host
+
+# get the proxy name (holding the Jet based repository)
+$proxyname = ($sourceRepository.Proxy -split ':')[0]
+
+$proxy = Get-VBOProxy -hostname $proxyname
+
+# disable the retention for the proxy
+Set-VBOConfigurationParameter -XPath "/Veeam/Archiver/RepositoryConfig" -Key "RetentionDisabled" -Value "True" -Proxy $proxy
+
+# start the migration process
+Start-VBODataMigration -From $sourceRepository -To $targetRepository -Job $selectedJob -SwitchJobToTargetRepository
+
+# -------------------
+# wait until migration run is finished. to check the status, start a new console and run:
+[Environment]::SetEnvironmentVariable("VEEAM_DATA_MIGRATION_ENABLED", "true")
+Get-VBODataMigration
+# -------------------
+
+# !! validate source and target with script !!
+
+# enable the retention for the proxy after validation is successful
+# Set-VBOConfigurationParameter -XPath "/Veeam/Archiver/RepositoryConfig" -Key "RetentionDisabled" -Value "False" -Proxy $proxy
